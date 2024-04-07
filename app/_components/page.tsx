294e486e0972344a4933f335cd4b5bc6f5415b52
Adison Cavani <adriansrodon@tuta.io>
2024-04-07 09:46:23 +0200

    feat: add minimal reproduction

---
+++ app/_components/page.tsx
@@ -0,0 +1,9 @@
+type Props = {
+  className?: string;
+};
+
+function PageComponent({ className }: Props) {
+  return <div className={className}>In progress... Stay tuned!</div>;
+}
+
+export default PageComponent;
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
-/** Add your relevant code here for the issue to reproduce */
-export default function Home() {
-  return null;
+import PageComponent from "./_components/page";
+
+function Page() {
+  return <PageComponent />;
 }
+
+export default Page;
